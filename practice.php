5e4290bfb7e3fd4be25faa96f6d3fad4ce6b76b0
kaneko <kaneko.yosuke0222@icloud.com>
2020-07-12 10:24:05 +0000

    課題提出２

--- practice.php
+++ practice.php
@@ -33,3 +33,93 @@ $calendar_2018 = [
 ];
 
 echo $calendar_2018["December"] . "\n";
+
+$height = 149;
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+} else {
+  echo "ご乗車になれます。" . "\n";
+}
+$height = 199;
+if ($height < 150) {
+  echo "150cm 未満の方はご乗車できません。";
+} else if ($height >= 200){
+  echo "200cm　以上の方はご乗車できません。";
+}else {
+  echo "ご乗車になれます。";
+}
+
+$weekday = "月曜日";
+switch ($weekday) {
+  case "月曜":
+   break;
+  case "木曜":
+   echo "可燃ゴミの日です。";
+   break;
+  case "水曜":
+   echo "資源ごみの日です。";
+   break;
+  default:
+   echo "回収はありません。";
+   break;
+}
+$fruits = array("apple", "orange", "lemon");
+  echo count($fruits);
+  for ($i = 0; $i < count($fruits); $i++) {
+    echo "要素は" . $fruits[$i];
+    echo "\n";
+}
+$a = 3;
+$b = 3;
+$c = "3";
+
+// $a と $b が等しいときに true　そうでなければ false を返す。
+var_dump($a == $b);
+//=> bool(true) が表示される。
+
+// $a と $b が等しくないときに true
+var_dump($a != $b);
+//=> bool(false)　が表示される。
+
+// $a が $b　より大きいときに　true、そうでなければ false を返す。
+var_dump($a > $b);
+//=> bool(false)が表示される
+
+// $a が $b 以上のときtrue、そうでなければ false を返す。
+var_dump($a >= $b);
+//=> bool(true) が表示される。
+
+// $a が $b より小さいときtrue、そうでなければ false を返す。
+var_dump($a < $b);
+//=> bool(false)が表示される
+
+//$a が $b より小さいか、または等しいときに true、そうでなければ false を返す。
+var_dump($a <= $b);
+//=> bool(true) が表示される。
+
+//$a が $c　とデータ型が等しく、かつ値も等しいときに true、そうでなければ false を返す。
+var_dump($a === $c);
+//=> bool(false)が表示される。
+
+//$a が $c　とデータ型が等しくないか、もしくは値が等しくないときに true、そうでなければ false を返す。
+var_dump($a !== $c);
+
+for($i = 0; $i < 10; $i++){
+  echo $i;
+}
+$total = 0;
+echo $total;
+//=> 0 と表示される。
+
+// $iが0から始まり、$iが１００以下の間繰り返し処理を行う。
+for ($i = 0; $i <= 100; $i++) {
+  $total += $i;
+}
+echo $total;
+$animals = array("dog", "cat", "panda");
+// $animals から一つずつ要素を取り出して、$animal に代入される
+
+foreach($animals as $animal){
+  echo "要素は" . $animal;
+  echo "\n";
+}
--- practice2.php
+++ practice2.php
@@ -1,37 +1,37 @@
 <?php
 
 $total = 0;
-    echo $total;
-    echo "\n";
+echo $total;
+echo "\n";
+for ($i = 0; $i <= 10000; $i++) {
+    $total += $i;
+}
+echo $total;
 
-    for ($i = 0; $i <= 10000; $i++) {
-        $total += $i;
-    }echo $total;
 echo "\n";
 
 $name = 'Yosuke Kaneko';
+if ($name == 'Yosuke Kaneko') {
+    echo "私は 'Yosuke Kaneko' です";
+}
+else {
+    echo "'Yosuke Kaneko'ではありません";
+}
 
-    if ($name == 'Yosuke Kaneko') {
-        echo "私は 'Yosuke Kaneko' です";
-    } else {
-        echo "'Yosuke Kaneko'ではありません";
-    }
 echo "\n";
 
 $fruits = array("strawberry", "mellon", "lemon", "apple", "orange");
-
-    foreach($fruits as $fruits){
-        echo $fruits;
-        echo "\n";
-    }
+foreach($fruits as $fruits){
+    echo $fruits;
+    echo "\n";
+}
 
 $start = 1;
 $end = 100;
 
-    for($i = $start; $i <= $end; $i++){
-        if($i % 5 == 0){
+for($i = $start; $i <= $end; $i++){
+    if ($i % 5 == 0) {
         echo $i;
         echo "\n";
-            
-        }
-    }    
+    }
+}    
